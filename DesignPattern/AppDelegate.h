4405634e2c769f73525dec7ad7a27c1ec13efec4
nfuadmin6 <nfuadmin6@nfuadmin6de-Mac-bo.local>
2014-03-14 11:00:20 +0800

    Initial Commit

---
+++ DesignPattern/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DesignPattern
+//
+//  Created by nfuadmin6 on 14/3/14.
+//  Copyright (c) 2014年 nfuadmin6. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DesignPattern/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  DesignPattern
+//
+//  Created by nfuadmin6 on 14/3/14.
+//  Copyright (c) 2014年 nfuadmin6. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
