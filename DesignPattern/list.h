86d3527ac9831c7250d4dc867363285132420d24
Bo-Shang-Chao <nfuadmin6@nfuadmin6de-Mac-bo.local>
2014-05-27 10:12:30 +0800

    design 2.0
    2.0

---
+++ DesignPattern/list.h
@@ -0,0 +1,13 @@
+//
+//  list.h
+//  DesignPattern
+//
+//  Created by nfuadmin6 on 14/5/27.
+//  Copyright (c) 2014年 nfuadmin6. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface list : NSObject
+
+@end
---
+++ DesignPattern/list.m
@@ -0,0 +1,13 @@
+//
+//  list.m
+//  DesignPattern
+//
+//  Created by nfuadmin6 on 14/5/27.
+//  Copyright (c) 2014年 nfuadmin6. All rights reserved.
+//
+
+#import "list.h"
+
+@implementation list
+
+@end
